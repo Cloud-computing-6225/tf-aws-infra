af4e8c5041322e18a49e46907374446faa07e325
nairashwin9297 <nair.ashw@northeastern.edu>
2024-10-17 13:22:13 -0400

    Added ec2 launch for given ami

--- variables.tf
+++ variables.tf
@@ -27,3 +27,38 @@ variable "vpc_name" {
 #   description = "Availability zones"
 #   type        = list(string)
 # }
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "app_port" {
+  description = "Port number for the web application"
+  type        = number
+  default     = 8080 # you can change this to your preferred port
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro" # You can choose a different instance type if needed
+}
+
+variable "key_pair_name" {
+  description = "Key pair for SSH access"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Name of the project for tagging resources"
+  type        = string
+}
+variable "allowed_ip" {
+  description = "The IP address allowed to access port 8080"
+  type        = string
+}
+variable "ami_name" {
+  description = "Name of the AMI to use"
+  type        = string
+}
