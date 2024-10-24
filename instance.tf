da2c40933f0ddb79d1298de2d82990811e8d98c1
nairashwin9297 <nair.ashw@northeastern.edu>
2024-10-24 06:03:36 -0400

    modified files for instance and added rds.tf

--- instance.tf
+++ instance.tf
@@ -66,7 +66,37 @@ data "aws_ami" "custom_ami" {
 }
 
 
-#To create an EC2 instance
+
+
+
+# resource "aws_instance" "web_app" {
+#   ami                    = data.aws_ami.custom_ami.id
+#   instance_type          = var.instance_type
+#   key_name               = var.key_pair_name
+#   vpc_security_group_ids = [aws_security_group.web_app_sg.id]
+#   subnet_id              = aws_subnet.public_subnets[0].id
+
+#   root_block_device {
+#     volume_size           = 25
+#     volume_type           = "gp2"
+#     delete_on_termination = true
+#   }
+
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     echo "DB_HOST=${aws_db_instance.csye6225_db.endpoint}" >> /etc/environment
+#     echo "DB_NAME=${var.db_name}" >> /etc/environment
+#     echo "DB_USER=${var.db_user}" >> /etc/environment
+#     echo "DB_PASSWORD=${var.db_password}" >> /etc/environment
+
+#   EOF
+
+#   disable_api_termination = false
+
+#   tags = {
+#     Name = "${var.project_name}-web-app-instance"
+#   }
+# }
 resource "aws_instance" "web_app" {
   ami                    = data.aws_ami.custom_ami.id
   instance_type          = var.instance_type
@@ -80,9 +110,38 @@ resource "aws_instance" "web_app" {
     delete_on_termination = true
   }
 
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+
+    # Create the directory if it doesn't exist
+    sudo mkdir -p /opt/webapp
+
+    # Create a .env file in /opt/webapp
+    cat <<EOL | sudo tee /opt/webapp/.env
+    DB_HOST=${aws_db_instance.csye6225_db.address}
+    DB_NAME=${var.db_name}
+    DB_USER=${var.db_user}
+    DB_PASSWORD=${var.db_password}
+    DB_PORT=3306
+    PORT=8080
+    EOL
+
+    # Debugging: print the created .env file
+    echo "Created .env file with the following contents:"
+    sudo cat /opt/webapp/.env
+
+    
+    sudo systemctl daemon-reload
+    sudo systemctl enable webapp.service
+    sudo systemctl start webapp.service
+  EOF
+
   disable_api_termination = false
 
   tags = {
     Name = "${var.project_name}-web-app-instance"
   }
+  depends_on = [aws_db_instance.csye6225_db]
 }
+
